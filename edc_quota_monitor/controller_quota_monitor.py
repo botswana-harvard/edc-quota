dc4b8c50b3a9c4edc8b92d97f1ca9a4b66e4a51b
ckgathi <ckgathi@bhp.org.bw>
2015-07-31 15:34:56 +0200

    quota

--- edc_quota_monitor/controller_quota_monitor.py
+++ edc_quota_monitor/controller_quota_monitor.py
@@ -8,19 +8,25 @@ class QoutaMonitor(object):
         pass
 
     def client_quota(self):
+        """Return client quota."""
         pass
 
     def master_quota(self):
+        """Return master server quota."""
         pass
 
     def quota_limit(self):
+        """Return the master quota limit."""
         pass
 
     def calculate_new_quotas(self):
+        """Calculate quota to allocate to clients."""
         pass
 
     def allocate_quota(self):
+        """Allocate client quota."""
         pass
 
     def by_pass_quota(self):
+        """Bypass quota limit."""
         pass
