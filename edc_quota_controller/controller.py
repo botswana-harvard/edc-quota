8eb851cfa8a39befe8571359e527e7d1b954aa68
erik van widenfelt <ew2789@gmail.com>
2015-08-02 16:42:14 +0200

    add initial controller code

--- edc_quota_controller/controller.py
+++ edc_quota_controller/controller.py
@@ -32,15 +32,15 @@ class Controller(object):
         self.clients_contacted = []
         self.register_all()
 
-    def register(self, client):
-        self.clients[client.name] = client
-
     def register_all(self):
         for client in Client.objects.filter(
                 app_label=self.app_label,
                 model_name=self.model_name,
                 is_active=True):
             self.register(client)
+
+    def register(self, client):
+        self.clients[client.name] = client
 
     def fetch_all(self):
         """Contacts all registered clients and updates the Quota model."""
@@ -68,7 +68,7 @@ class Controller(object):
 
     def update_all(self):
         """Contacts all registered clients and sets their new quota targets."""
-        quota_history = self.calculate_new_quota()
+        quota_history = self.calculate()
         for name in quota_history.clients_contacted.split(','):
             self.clients.get(name).new_quota_target = quota_history.new_quota_target
             self.clients.get(name).new_quota_expires = quota_history.new_quota_expires
@@ -76,7 +76,7 @@ class Controller(object):
             # add put request here
             pass
 
-    def calculate_new_quota(self):
+    def calculate(self):
         """Calculates new targets, updates QuotaHistory and returns the new QuotaHistory instance."""
         quota_history = QuotaHistory.objects.filter(quota=self.quota).last()
         quota_history.new_quota_target = '?'  # add calculation for a new target for all contacted clients
