580940012894e224c8482ad5dad9a174c60da205
erik van widenfelt <ew2789@gmail.com>
2015-08-04 16:20:49 +0200

    separate make, unmake code algorithm

--- edc_quota/tests.py
+++ edc_quota/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .override import Override
+from .override import Override, CodeError
 
 
 class TestOverride(TestCase):
@@ -15,24 +15,23 @@ class TestOverride(TestCase):
     def test_combination(self):
         override = Override()
         self.assertFalse(override.validate_combination())
-        code, confirmation_code = override.create_confirmation_code()
+        code, confirmation_code = override.make_confirmation_code()
         self.assertIsNotNone(confirmation_code)
         self.assertTrue(override.validate_combination(code, confirmation_code))
 
     def test_combination_code_returns_code(self):
         override = Override()
         code1 = override.code
-        code2, _ = override.create_confirmation_code(code1)
+        code2, _ = override.make_confirmation_code(code1)
         self.assertEqual(code1, code2)
 
     def test_combination_manually(self):
         override = Override()
         code1 = override.code
-        _, confirmation_code = override.create_confirmation_code(code1)
+        _, confirmation_code = override.make_confirmation_code(code1)
         self.assertTrue(override.validate_combination(code1, confirmation_code))
 
     def test_combination_blank(self):
         override = Override()
         override.code = ''
-        code, confirmation_code = override.create_confirmation_code('')
-        self.assertFalse(override.validate_combination(code, confirmation_code))
+        self.assertRaises(CodeError, override.make_confirmation_code, '')
