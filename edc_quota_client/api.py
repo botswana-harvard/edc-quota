7796e344ecdd6ddb42018cba4cbfbaf659c2cccf
ckgathi <ckgathi@bhp.org.bw>
2015-08-05 13:23:18 +0200

    pep8 fix

--- edc_quota_client/api.py
+++ edc_quota_client/api.py
@@ -9,15 +9,7 @@ class QuotaResource(ModelResource):
     class Meta:
         resource_name = 'quota'
         queryset = Quota.objects.all()
-        fields = [
-            'target',
-            'model_count',
-            'id',
-            'quota_datetime',
-            'app_label',
-            'model_name',
-            'expires_datetime'
-        ]
+        fields = ['target', 'model_count', 'id', 'quota_datetime', 'app_label', 'model_name', 'expires_datetime']
         list_allowed_methods = ['get', 'post']
         detail_allowed_methods = ['get', 'post', 'put']
         filtering = {
