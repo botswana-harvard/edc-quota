71e3c73ea21abcf8321392d1eab777bd7558bc1d
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-08-10 09:46:12 +0200

    django 1.6 compatible admin register

--- edc_quota_client/admin.py
+++ edc_quota_client/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Quota
 
 
-@admin.register(Quota)
 class QuotaAdmin(admin.ModelAdmin):
     date_hierarchy = 'quota_datetime'
     list_display = ('model_name', 'app_label', 'target', 'expires_datetime', 'model_count', 'is_active')
     list_filter = ('is_active', 'app_label', 'expires_datetime', 'quota_datetime')
     search_fields = ('model_name', )
+admin.site.register(Quota, QuotaAdmin)
