ce1574a30829ac288c05af6fd8b28447ef31d667
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-08-30 15:09:49 +0200

    client admin and management command

--- edc_quota_controller/admin.py
+++ edc_quota_controller/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Quota, QuotaHistory
+from django.contrib.admin.templatetags.admin_list import date_hierarchy
+from edc_quota_controller.models import Client
 
 
 @admin.register(Quota)
@@ -17,3 +19,14 @@ class QuotaHistoryAdmin(admin.ModelAdmin):
     list_display = ('quota', 'expires_datetime', 'last_contact', 'clients_contacted', 'model_count')
     list_filter = ('expires_datetime', 'last_contact')
     search_fields = ('clients_contacted', )
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    date_hierarchy = 'expires_datetime'
+    list_display = (
+        'hostname', 'app_label', 'model_name', 'port', 'last_contact', 'target',
+        'expires_datetime', 'is_active'
+    )
+    list_filter = ('app_label', 'model_name', 'is_active', 'expires_datetime')
+    search_fields = ('app_label', 'model_name')
