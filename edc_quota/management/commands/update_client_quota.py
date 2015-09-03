877c44efbcfd485e5fb932afacc9a936b74ecc21
erikvw <ew2789@gmail.com>
2015-09-03 13:40:32 +0200

    management command, fix filter

--- edc_quota/management/commands/update_client_quota.py
+++ edc_quota/management/commands/update_client_quota.py
@@ -1,6 +1,6 @@
 import sys
 
-from django.utils import timezone
+from datetime import date
 from django.core.management.base import BaseCommand
 
 from edc_quota.controller import Controller, ControllerQuota
@@ -8,34 +8,30 @@ from edc_quota.controller import Controller, ControllerQuota
 
 class Command(BaseCommand):
 
-    args = ('--clients', '--post_client')
-
-    help = 'Update the client quota'
-
-    def add_arguments(self, parser):
-
-        parser.add_argument(
-            '--clients', action='store_true', default=False,
-            help='Update one or more specific clients'
-        )
+    help = 'Update the quotas'
 
     def handle(self, *args, **options):
         self.clients = None
         sys.stdout.write('Begin ...\n')
         sys.stdout.flush()
-        self.clients = args[0].split(',')
-        sys.stdout.write('Updating clients {} only\n'.format(args[0]))
+        try:
+            self.clients = args[0].split(',')
+            sys.stdout.write('Updating clients \'{}\' only\n'.format('\', \''.join(self.clients)))
+        except IndexError:
+            pass
         sys.stdout.flush()
-        for quota in ControllerQuota.objects.filter(is_active=True, expiration_date__lte=timezone.now()):
-            try:
-                sys.stdout.flush()
-                controller = Controller(quota, clients=self.clients)
-                sys.stdout.write('Updating {}\n'.format(quota))
-                controller.get_all()
-                controller.post_all() 
-                sys.stdout.write('Done updating {}\n'.format(quota))
-                sys.stdout.flush()
-            except ControllerQuota.DoesNotExist:
-                pass
+        quotas = ControllerQuota.objects.filter(is_active=True, expiration_date__gte=date.today())
+        if quotas.count() == 0:
+            sys.stdout.write('Nothing to do! Try creating a controller quota.\n')
+        else:
+            sys.stdout.write('Processing {} controller quota.\n'.format(quotas.count()))
+        for quota in quotas:
+            sys.stdout.flush()
+            controller = Controller(quota, clients=self.clients)
+            sys.stdout.write('Updating {}\n'.format(quota))
+            controller.get_all()
+            controller.post_all()
+            sys.stdout.write('Done updating {}\n'.format(quota))
+            sys.stdout.flush()
         sys.stdout.write('Done.\n')
         sys.stdout.flush()
