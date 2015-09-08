b0923311e0e397ce1e253d5089b882cd9ea3be89
erikvw <ew2789@gmail.com>
2015-09-08 15:03:44 +0200

    remove request_code from readonly

--- edc_quota/override/admin.py
+++ edc_quota/override/admin.py
@@ -5,5 +5,4 @@ from .models import OverrideModel
 
 class OverrideAdmin(admin.ModelAdmin):
     fields = ('request_code', 'override_code', )
-    readonly_fields = ('request_code', )
 admin.site.register(OverrideModel, OverrideAdmin)
