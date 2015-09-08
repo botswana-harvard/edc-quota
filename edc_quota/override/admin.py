cb23c1a6460a4f49097da2a79eb39971482d9de6
erikvw <ew2789@gmail.com>
2015-09-08 14:56:55 +0200

    sow request code in admin

--- edc_quota/override/admin.py
+++ edc_quota/override/admin.py
@@ -4,5 +4,6 @@ from .models import OverrideModel
 
 
 class OverrideAdmin(admin.ModelAdmin):
-    pass
+    fields = ('request_code', 'override_code', )
+    readonly_fields = ('request_code', )
 admin.site.register(OverrideModel, OverrideAdmin)
