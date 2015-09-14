d93010ba33ec804c5436b12c05ada49ae344253a
erikvw <ew2789@gmail.com>
2015-09-14 17:17:08 +0200

    remove admin autodiscover

--- edc_quota/models.py
+++ edc_quota/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 from tastypie.models import create_api_key
 
-from .client.models import Quota
-from .controller.models import Client, ControllerQuota, ControllerQuotaHistory
-from .override.models import OverrideModel
+# from .client.models import Quota
+# from .controller.models import Client, ControllerQuota, ControllerQuotaHistory
+# from .override.models import OverrideModel
 
 models.signals.post_save.connect(create_api_key, sender=User)
--- edc_quota/override/forms.py
+++ edc_quota/override/forms.py
@@ -1,13 +1,13 @@
-# from django import forms
-# 
-# 
-# class OverridetForm(forms.Form):
-# 
-#     override_request = forms.CharField(required=True)
-#     override_code = forms.CharField()
-# 
+from django import forms
+
+
+class OverridetForm(forms.Form):
+
+    override_request = forms.CharField(required=True)
+    override_code = forms.CharField()
+
 #     def clean(self):
 #         cleaned_data = super(OverridetForm, self).clean()
-#         
+#
 #         if cleaned_data.get('override_request'):
-#             clean_data.
+#             clean_data[]
